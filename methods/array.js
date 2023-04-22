fa0e4fe19612dce7a6dd57cf52028b488a5ed1c2
Vadim Cherepanov <xenos26@gmail.com>
2023-04-22 12:11:06 +0700

    feat: add methods

---
+++ methods/array.js
@@ -0,0 +1,11 @@
+let array = [1, "Vadim", { age: 34 }, 4.5,]
+
+//push(...item) add item in the end
+array.push(48);
+//console.log(array.push(48)); // return new length of array
+// console.log(array)
+
+array.pop() //removes last element of array
+//console.log(array)
+console.log(array.pop())
+
