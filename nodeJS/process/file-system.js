0c16706dc46dcc4c7fa3fe754a87f93774f83d79
Vadim Cherepanov <xenos26@gmail.com>
2023-04-29 22:49:23 +0700

    feat: add rmdir

--- nodeJS/process/file-system.js
+++ nodeJS/process/file-system.js
@@ -2,10 +2,16 @@ const fs = require('fs')
 const path = require('path')
 
 // fs.mkdirSync(path.resolve('dir', 'dir2', 'dir3'), { recursive: true })
-fs.mkdir(path.resolve(__dirname, 'dir'), (err) => {
+// fs.mkdir(path.resolve(__dirname, 'dir'), (err) => {
+//   if (err) {
+//     console.log(err)
+//     return
+//   }
+//   console.log("Directory created")
+// })
+
+fs.rmdir(path.resolve('dir'), (err) => {
   if (err) {
-    console.log(err)
-    return
+    throw err;
   }
-  console.log("Directory created")
-}, { recursive: true })
+})
