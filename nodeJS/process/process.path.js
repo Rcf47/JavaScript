f9e886a42851d590be2bda9283d1b0b739bf7442
Vadim Cherepanov <xenos26@gmail.com>
2023-04-29 22:21:49 +0700

    feat: add url

--- nodeJS/process/process.path.js
+++ nodeJS/process/process.path.js
@@ -1,8 +1,12 @@
 const path = require('path')
 
-console.log(path.join(__dirname, 'first', 'second'));
-console.log(path.join(__dirname, '..', '..'));
-console.log(path.resolve('first'))
+// console.log(path.join(__dirname, 'first', 'second'));
+// console.log(path.join(__dirname, '..', '..'));
+// console.log(path.resolve('first'))
 
 let fullPath = path.resolve('first', 'second')
-console.log(path.parse(fullPath))
+// console.log(path.parse(fullPath))
+
+const siteUrl = 'http://localhost:8080/users?id=5123'
+const url = new URL(siteUrl)
+console.log(url)
