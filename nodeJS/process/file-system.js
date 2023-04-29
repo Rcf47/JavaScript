acb40dd6e615e64d969f18e93f64490e59dbd3ef
Vadim Cherepanov <xenos26@gmail.com>
2023-04-29 22:45:42 +0700

    feat: add async mkdir

--- nodeJS/process/file-system.js
+++ nodeJS/process/file-system.js
@@ -1,4 +1,11 @@
 const fs = require('fs')
 const path = require('path')
 
-fs.mkdirSync(path.resolve('dir', 'dir2', 'dir3'), { recursive: true })
+// fs.mkdirSync(path.resolve('dir', 'dir2', 'dir3'), { recursive: true })
+fs.mkdir(path.resolve(__dirname, 'dir'), (err) => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log("Directory created")
+}, { recursive: true })
