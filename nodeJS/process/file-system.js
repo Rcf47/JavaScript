a8fe9c9d2a0ebc9f0ec7739a1b6247c1e857a27d
Vadim Cherepanov <xenos26@gmail.com>
2023-04-29 23:29:47 +0700

    feat: rm file

--- nodeJS/process/file-system.js
+++ nodeJS/process/file-system.js
@@ -1,6 +1,6 @@
-const fs = require('fs')
-const path = require('path')
-const fsPromise = require('fs/promises')
+const fs = require("fs");
+const path = require("path");
+const fsPromise = require("fs/promises");
 
 // fs.mkdirSync(path.resolve('dir', 'dir2', 'dir3'), { recursive: true })
 // fs.mkdir(path.resolve(__dirname, 'dir'), (err) => {
@@ -29,4 +29,11 @@ const fsPromise = require('fs/promises')
 // fsPromise.writeFile(path.resolve('test.txt'), 'Hello World \n')
 //   .then(fsPromise.appendFile(path.resolve('test.txt'), 'Hello from Append'))
 //   .catch(err => console.log(err))
-fsPromise.readFile(path.resolve('test.txt'), { encoding: 'utf8' }).then(data => console.log(data)).catch(err => console.log(err))
+//
+// fsPromise
+//   .readFile(path.resolve("test.txt"), { encoding: "utf8" })
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+//
+let file = path.resolve('test.txt')
+// fsPromise.rm(file).then(() => console.log(`${file} was removed`)).catch(err => console.log(err))
