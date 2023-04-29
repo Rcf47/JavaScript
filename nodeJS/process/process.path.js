f2922f4e7f62df1a18650cbdd0d2b18725d9d485
Vadim Cherepanov <xenos26@gmail.com>
2023-04-29 22:17:32 +0700

    feat: add fullPath with parse

--- nodeJS/process/process.path.js
+++ nodeJS/process/process.path.js
@@ -3,3 +3,6 @@ const path = require('path')
 console.log(path.join(__dirname, 'first', 'second'));
 console.log(path.join(__dirname, '..', '..'));
 console.log(path.resolve('first'))
+
+let fullPath = path.resolve('first', 'second')
+console.log(path.parse(fullPath))
