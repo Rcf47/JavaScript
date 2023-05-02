34112d2b17a6867a0eb2dd282a04980b3cbfb5a8
Vadim Cherepanov <xenos26@gmail.com>
2023-05-02 18:53:59 +0700

    feat: add expressLayout with EJS

---
+++ nodeJS/process/expressLayout.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`Server is working on: http://localhost:${PORT}`)
+})
