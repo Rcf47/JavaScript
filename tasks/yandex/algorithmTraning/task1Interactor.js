607ae60cf8a31560391a2f10848fdf51fb7caa51
Vadim Cherepanov <xenos26@gmail.com>
2023-05-03 22:05:06 +0700

    feat: add decision for task 1

--- tasks/yandex/algorithmTraning/task1Interactor.js
+++ tasks/yandex/algorithmTraning/task1Interactor.js
@@ -5,7 +5,56 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Enter your name: ', name => {
-  console.log(`Hello ${name}`)
+
+let r = undefined
+let i = undefined
+let c = undefined
+let result
+
+rl.on('line', arg => {
+  if (r === undefined) {
+    r = Number(arg)
+    return
+  }
+  if (i === undefined) {
+    i = Number(arg)
+    return
+  }
+  if (c === undefined) {
+    c = Number(arg)
+  }
+  switch (i) {
+    case 0:
+      if (r !== 0) {
+        result = 3
+      } else {
+        result = c
+      }
+
+      break;
+    case 1:
+      result = c
+
+      break;
+    case 4:
+      if (r !== 0) {
+        result = 3
+      } else {
+        result = 4
+      }
+
+      break;
+    case 6:
+      result = 0
+
+      break;
+    case 7:
+      result = 1
+      break
+    default:
+      result = i
+      break;
+  }
+  console.log(result)
   rl.close
 });
