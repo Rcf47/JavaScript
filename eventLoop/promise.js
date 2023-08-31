ed58f740c963b5c2b8fe557f6a6aa061b47b16a9
Vadim Cherepanov <xenos26@gmail.com>
2023-08-31 12:04:12 +0700

    docs: add experiments with promise

--- eventLoop/promise.js
+++ eventLoop/promise.js
@@ -1,2 +1,21 @@
-new Promise(resolve => setTimeout(resolve)).then(() => console.log(4))
-Promise.resolve(5).then(console.log)
+new Promise(resolve => setTimeout(resolve)).then(() => console.log("setTimeout from first promis"))
+
+let prom = new Promise((resolve, reject) => {
+  resolve("🤡")
+})
+
+prom
+  .then((smile) => { console.log(smile) })
+
+Promise.resolve("🎅").then((santa) => {
+  console.log(santa)
+  return santa
+})
+  .then(() => Promise.reject("no santa"))
+  .finally(() => { console.log("from finally") })
+  .catch((error) => {
+    console.log(error)
+    return "hello world"
+  }).then((
+    santa
+  ) => { console.log(santa) })
